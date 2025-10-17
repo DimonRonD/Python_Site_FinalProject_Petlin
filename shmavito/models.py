3152613e1f79ad295a39e62b6ce39d749e3588ef
Dmitry Petlin <DimonRonD@gmail.com>
2025-10-17 14:21:06 +0300

    Initial commit

--- shmavito/models.py
+++ shmavito/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomerStatus(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название статуса')
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Город')
+
+class Customer(AbstractUser):
+    city = models.ForeignKey(City, on_delete=models.PROTECT, related_name='cities', verbose_name='Город')
+    phone = models.CharField(max_length=15, verbose_name='Номер телефона')
+    tg_name = models.CharField(max_length=50, verbose_name='Имя пользователя Telegram')
+    tg_id = models.IntegerField(verbose_name='ID в Telegram')
+    status = models.ForeignKey(CustomerStatus, on_delete=models.PROTECT, related_name='customers', verbose_name='Статус пользователя')
+
+class Good(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Название товара')
+    description = models.TextField(verbose_name='Описание товара')
+
+class ImageStatus(models.Model):
+    name = models.CharField(max_length=25, verbose_name='Статус фотографии')
+
+class GoodImage(models.Model):
+    good = models.ForeignKey(Good, on_delete=models.CASCADE, related_name='images', verbose_name='Товар')
+    image = models.ImageField(verbose_name='Фотография товара')
+    status =models.ForeignKey(ImageStatus, on_delete=models.PROTECT, related_name='images', verbose_name='Статус фотографии')
+
+class Advertisement(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name='adv', verbose_name='Пользователь')
+    good = models.ForeignKey(Good, on_delete=models.PROTECT, related_name='adv', verbose_name='Товар')
+    sdate = models.DateField(verbose_name='Дата начала предложения')
+    edate = models.DateField(verbose_name='Дата окончания предложения')
+    name = models.CharField(max_length=200, verbose_name='Название предложения')
+    description = models.TextField(verbose_name='Описание предложения')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Стоимость предложения')
+    moderate = models.BooleanField(default=False, verbose_name='Прошел модерацию')
+    city = models.ForeignKey(City, on_delete=models.PROTECT, related_name='cities', verbose_name='Город')
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Статус предложения')
+
+class Order(models.Model):
+    ad = models.ForeignKey(Advertisement, on_delete=models.CASCADE, related_name='orders', verbose_name='Предложение')
+    order_date = models.DateField(verbose_name='Дата заказа')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders', verbose_name='Пользователь')
+    status = models.ForeignKey(OrderStatus, on_delete=models.PROTECT, related_name='orders', verbose_name='Статус заказа')
+    sdate = models.DateField(verbose_name='Дата начала заказа')
+    edate = models.DateField(verbose_name='Дата окончания заказа')
+
+class Comment(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='comments', verbose_name='Пользователь')
+    ad = models.ForeignKey(Advertisement, on_delete=models.CASCADE, related_name='comments', null=True, verbose_name='Предложение')
+    comment = models.TextField(null = False, verbose_name='Комментарий')
+    photo = models.ImageField(null=True, verbose_name='Фотография к комментарию')
+
